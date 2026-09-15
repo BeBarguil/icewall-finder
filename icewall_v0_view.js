642b6eca71db14a45f18cf8ac16bf0d6053726d7
Beatriz Barguil <beatrizz.barguil@gmail.com>
2026-09-15 12:04:13 -0300

    Delete icewall_v0_view.js

--- icewall_v0_view.js
+++
@@ -1,5 +0,0 @@
-// Fast viewer for the exported asset (no recomputation, no AOI edge).
-var cls = ee.Image('projects/icewall-ad/assets/icewall_v0_classified');
-Map.setOptions('SATELLITE');
-Map.centerObject(cls, 10);
-Map.addLayer(cls, {min: 1, max: 3, palette: ['9ecae1', 'ff3300', 'ffff00']}, 'Ice walls');
